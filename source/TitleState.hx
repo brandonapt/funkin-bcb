288ae0289f8722a20e4dca66baaff06ced475395
Brandon! <brandoge91@gmail.com>
2021-09-08 17:20:48 -0400

    this looks weird idk why i added

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -432,7 +432,6 @@ class TitleState extends MusicBeatState
 	{
 		super.beatHit();
 
-		FlxTween.tween(FlxG.camera, {zoom: 1.05}, 0.3, {ease: FlxEase.quadOut, type: BACKWARD});
 		if (logoBl.visible = true)
 			logoBl.animation.play('bump');
 		danceLeft = !danceLeft;

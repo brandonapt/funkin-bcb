d4989d5d71e6da7a774cd2577c5aaa9cb063753f
Brandon! <brandoge91@gmail.com>
2021-09-08 17:19:18 -0400

    i added early hit windows to make kadedev happy lo

--- source/Note.hx
+++ source/Note.hx
@@ -235,8 +235,7 @@ class Note extends FlxSkewedSprite
 			{
 				// The * 0.5 is so that it's easier to hit them too late, instead of too early
 				if (strumTime > Conductor.songPosition - Conductor.safeZoneOffset
-					&& strumTime < Conductor.songPosition + (Conductor.safeZoneOffset * 0.5))
-					canBeHit = true;
+					&& strumTime < Conductor.songPosition + (Conductor.safeZoneOffset * (isSustainNote ? 0.5 : 1)))					canBeHit = true;
 				else
 					canBeHit = false;
 	

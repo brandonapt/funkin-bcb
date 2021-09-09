4ecdc7a13b68c9d63373a97154c2ef77e922ad6e
Brandon! <brandoge91@gmail.com>
2021-09-09 18:38:53 -0400

    shhhhhhh

---
+++ source/GameJolt.hx
@@ -0,0 +1,11 @@
+package;
+import flixel.addons.api.FlxGameJolt;
+
+class GameJolt
+{
+    public function authenticateUser(username:String, userToken:String, callbackfunction:Dynamic)
+        {
+            authUser(username, userToken, callbackfunction)
+            return initialized
+        }
+}
